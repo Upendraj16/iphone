d95989ce88036110becc484f1b8c3e349ed3ad04
root <root@caronaVM.qwfunwwfkdeunajjy21omnmfjd.bx.internal.cloudapp.net>
2020-04-15 06:16:01 +0000

    appdevlp project

---
+++ appdevlp/src/main/java/iphonecompany/App.java
@@ -0,0 +1,13 @@
+package iphonecompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
