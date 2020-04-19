cd9d5c27ff489b597e596818774a4581db6ab71c
root <root@mvnserver.04aax4h3nvfefjspwd1oce32df.cx.internal.cloudapp.net>
2020-04-19 12:50:28 +0000

    micromax project

---
+++ microsimdevlp/src/main/java/micromax/App.java
@@ -0,0 +1,13 @@
+package micromax;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
